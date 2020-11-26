79343e36fbd4612af245bad4cdb742d0c9f1b295
Max Nicolai <maxvnicolai@gmail.com>
2020-11-25 16:20:52 -0800

    added router, placeholder pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import  NavBar from './components/Nav/index.js'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- portfolio/src/pages/Portfolio.js
+++ portfolio/src/pages/Portfolio.js
@@ -1,1 +1,5 @@
 import React from 'react';
+
+export default function Portfolio(){
+  return (<div>PortfolioTest</div>)
+}
