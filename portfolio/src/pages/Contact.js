341b20615645a7d3c09f6bc387fe8c0de0778d2f
Max Nicolai <maxvnicolai@gmail.com>
2020-11-28 16:39:19 -0800

    rebuilt contact page

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
+import ContactForm from '../components/Form'
 
 export default function Contact(){
-  return (<div>
-      Contact Test
-    </div>)
+  return (
+    <Container>
+      <Row className="justify-content-start">
+        <Col md className="head">
+          <h2>Contact</h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-start" id="con">
+       <ContactForm />
+      </Row>
+    </Container>
+  )
 }
