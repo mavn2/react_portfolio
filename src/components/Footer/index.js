7b3b211c5a47e1617a6c0dc69f60624fa2998717
Max Nicolai <maxvnicolai@gmail.com>
2020-12-11 16:54:28 -0800

    removed console.log

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,46 +1,57 @@
 // Dependencies
 import React from 'react';
-import { Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 // Import Resume
-import resume from "../../assets/Resume.pdf"
+import resume from '../../assets/Resume.pdf';
 
 //  Exports a row containing contact info
 // Since content should be consistent, defined here rather then entered as props
-export default function Footer(){
-  return(
+export default function Footer() {
+  return (
     <Row className="foot">
       <Col sm={2}>
         <h5 className="text-center">
-          <a href="https://github.com/mavn2" className="card-link" rel="noreferrer" target="_blank">
+          <a
+            href="https://github.com/mavn2"
+            className="card-link"
+            rel="noreferrer"
+            target="_blank"
+          >
             Github
           </a>
         </h5>
       </Col>
       <Col sm={2}>
         <h5 className="text-center">
-          <a href="https://www.linkedin.com/in/max-nicolai/" className="card-link" rel="noreferrer" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/max-nicolai/"
+            className="card-link"
+            rel="noreferrer"
+            target="_blank"
+          >
             LinkedIn
           </a>
         </h5>
       </Col>
       <Col sm={2}>
         <h5 className="text-center">
-          <a href={resume} className="card-link" rel="noreferrer" target="_blank">
+          <a
+            href={resume}
+            className="card-link"
+            rel="noreferrer"
+            target="_blank"
+          >
             Resume
           </a>
         </h5>
       </Col>
       <Col sm={2}>
-        <h5 className="text-center">
-          (206) 450-1913
-        </h5>
+        <h5 className="text-center">(206) 450-1913</h5>
       </Col>
       <Col sm={2}>
-        <h5 className="text-center">
-          maxvnicolai@gmail.com
-        </h5>
+        <h5 className="text-center">maxvnicolai@gmail.com</h5>
       </Col>
     </Row>
   );
-};
+}
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -10,7 +10,6 @@ import projects from '../assets/projects';
 // Using react-bootstrap's CardColumns component greatly simplifies the code for this page, replacing Rows/Cols I defined in original portfolio
 // Each item component is a card that takes a project's info as props,
 export default function Portfolio() {
-  console.log(projects);
   return (
     <Container className="main">
       <Header page={'Portfolio'} />
